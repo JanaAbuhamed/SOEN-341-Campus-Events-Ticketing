fc56a47d7e4722c0b7c4d91b59175788b5218ae4
JanaAbuhamed <jana.abuhamed05@gmail.com>
2025-10-04 05:21:49 -0400

    Attempts at connecting backend to database
    The user model is ready. However, there seems to be an error that does not allow connection to proceed.

--- student_event/settings.py
+++ student_event/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +77,7 @@ DATABASES = {
         'USER': 'campus_event',
         'PASSWORD': 'campus_event',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '3307',
     }
 }
 
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# tells Django which model to use for authentication (logins, passwords, sessions, etc.).
+AUTH_USER_MODEL = 'main.User'
