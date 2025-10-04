88efecae9c40c67c017f47738ae8cef3e3c688c6
lynesdd <lynesdd@gmail.com>
2025-10-04 09:16:03 -0400

    fix: update URL import to use main.api.urls

--- student_event/urls.py
+++ student_event/urls.py
@@ -23,5 +23,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('main.urls')),
+    path('api/', include('main.api.urls')),
 ]
